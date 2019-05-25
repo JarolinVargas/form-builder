e89a053ba1452060ea5aa5512cd2bfb43b1c6da7
JarolinVargas <jarolinvargas@gmail.com>
2019-05-25 03:36:56 -0400

    fixed edit text state update

--- src/output-resources/items.js
+++ src/output-resources/items.js
@@ -6,7 +6,8 @@ import AlignerCol1 from './item-components/AlignerCol1';
 export function Header1(props) {
     return (
         <AlignerCol1 outlineItem={props.outlineItem} itemClick={props.itemClick} itemDoubleClick={props.itemDoubleClick}>
-            <h1 contentEditable={props.contentEditableOn} onBlur={props.itemTextBlur}>Contact Us Today!</h1>
+            <h1 contentEditable={props.contentEditableOn} onBlur={props.itemTextBlur} data-text-prop='text1'>{props.text1}</h1>
+            <p contentEditable={props.contentEditableOn} onBlur={props.itemTextBlur} data-text-prop='text2'>{props.text2}</p>
         </AlignerCol1>
     );
 }
