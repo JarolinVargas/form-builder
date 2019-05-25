b36ab50164d3effd34d6804503d85b2cdaa6db67
JarolinVargas <jarolinvargas@gmail.com>
2019-05-25 02:44:02 -0400

    initial commit

--- src/output-resources/items.js
+++ src/output-resources/items.js
@@ -6,7 +6,7 @@ import AlignerCol1 from './item-components/AlignerCol1';
 export function Header1(props) {
     return (
         <AlignerCol1 outlineItem={props.outlineItem} itemClick={props.itemClick} itemDoubleClick={props.itemDoubleClick}>
-            dskfhs
+            <h1 contentEditable={props.contentEditableOn} onBlur={props.itemTextBlur}>Contact Us Today!</h1>
         </AlignerCol1>
     );
 }
